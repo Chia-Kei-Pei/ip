58b73671cdb161f36bc4a6bd7ee1ac26e157cdc1
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-08-20 20:24:48 +0800

    Change name from "Duke" to "B E R T"

---
+++ src/main/java/Bert.java
@@ -0,0 +1,19 @@
+public class Bert {
+    public static void main(String[] args) {
+        String banner = """
+ ____     ___  ____  ______
+|    \\   /  _]|    \\|      |
+|  o  ) /  [_ |  D  )      |
+|     ||    _]|    /|_|  |_|
+|  O  ||   [_ |    \\  |  | 
+|     ||     ||  .  \\ |  | 
+|_____||_____||__|\\_| |__| 
+""";
+        System.out.println("------------------------------------------------------------");
+        System.out.println(banner);
+        System.out.println("I am  B E R T.");
+        System.out.println("What'd you need?");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Goodbye.");
+    }
+}
