1463562942fc23aa073004f0559414180ca79476
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-08-21 15:49:42 +0800

    Add exceptions to invalid command use

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -5,7 +5,7 @@ import datatypes.Todo;
 import java.util.ArrayList;
 
 public class Bert {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         String[] command;
         ArrayList<Todo> itemList = new ArrayList<>();
 
@@ -31,92 +31,133 @@ public class Bert {
             StringBuilder itemBuilder;
             String item;
 
-            if (command[0].contentEquals("todo")) {
-                itemBuilder = new StringBuilder();
-                for (int i = 1; i < command.length; i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                item = itemBuilder.toString().trim();
+            try {
+                if (command[0].contentEquals("todo")) {
+                    int i = 1;
+                    itemBuilder = new StringBuilder();
 
-                Todo todo = new Todo(item);
-                itemList.add(todo);
-                IO.println("Added todo");
-                IO.println(todo);
-            } else if (command[0].contentEquals("deadline")) {
-                int i = 1;
-                itemBuilder = new StringBuilder();
-                for (; !command[i].contentEquals("/by"); i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                item = itemBuilder.toString().trim();
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("Name of todo should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    item = itemBuilder.toString().trim();
 
-                i++;
-                itemBuilder = new StringBuilder();
-                for (; i < command.length; i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                String byDate = itemBuilder.toString().trim();
+                    Todo todo = new Todo(item);
+                    itemList.add(todo);
+                    IO.println("Added todo");
+                    IO.println(todo);
+                } else if (command[0].contentEquals("deadline")) {
+                    int i = 1;
+                    itemBuilder = new StringBuilder();
 
-                Deadline deadline = new Deadline(item, byDate);
-                itemList.add(deadline);
-                IO.println("Added deadline");
-                IO.println(deadline);
-            } else if (command[0].contentEquals("event")) {
-                int i = 1;
-                itemBuilder = new StringBuilder();
-                for (; !command[i].contentEquals("/from"); i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                item = itemBuilder.toString().trim();
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("Name of deadline should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        if (command[i].contentEquals("/by")) {
+                            break;
+                        }
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    item = itemBuilder.toString().trim();
 
-                i++;
-                itemBuilder = new StringBuilder();
-                for (; !command[i].contentEquals("/to"); i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                String fromDate = itemBuilder.toString().trim();
+                    i++;
+                    itemBuilder = new StringBuilder();
 
-                i++;
-                itemBuilder = new StringBuilder();
-                for (; i < command.length; i++) {
-                    itemBuilder.append(" ");
-                    itemBuilder.append(command[i]);
-                }
-                String toDate = itemBuilder.toString().trim();
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("ByDate of deadline should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    String byDate = itemBuilder.toString().trim();
 
-                Event event = new Event(item, fromDate, toDate);
-                itemList.add(event);
-                IO.println("Added event");
-                IO.println(event);
-            } else if (command[0].contentEquals("bye")
-                    || command[0].contentEquals("exit")
-                    || command[0].contentEquals("quit")) {
-                IO.println("Goodbye.");
-                return;
-            } else if (command[0].contentEquals("list")) {
-                if (itemList.size() == 0) {
-                    IO.println("List is empty.");
+                    Deadline deadline = new Deadline(item, byDate);
+                    itemList.add(deadline);
+                    IO.println("Added deadline");
+                    IO.println(deadline);
+                } else if (command[0].contentEquals("event")) {
+                    int i = 1;
+                    itemBuilder = new StringBuilder();
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("Name of event should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        if (command[i].contentEquals("/from")) {
+                            break;
+                        }
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    item = itemBuilder.toString().trim();
+
+                    i++;
+                    itemBuilder = new StringBuilder();
+
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("FromDate of event should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        if (command[i].contentEquals("/to")) {
+                            break;
+                        }
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    String fromDate = itemBuilder.toString().trim();
+
+                    i++;
+                    itemBuilder = new StringBuilder();
+
+                    if (i > command.length - 1) {
+                        throw new IllegalArgumentException("ToDate of event should not be empty");
+                    }
+                    for (; i < command.length; i++) {
+                        itemBuilder.append(" ");
+                        itemBuilder.append(command[i]);
+                    }
+                    String toDate = itemBuilder.toString().trim();
+
+                    Event event = new Event(item, fromDate, toDate);
+                    itemList.add(event);
+                    IO.println("Added event");
+                    IO.println(event);
+                } else if (command[0].contentEquals("bye")
+                        || command[0].contentEquals("exit")
+                        || command[0].contentEquals("quit")) {
+                    IO.println("Goodbye.");
                     return;
-                }
+                } else if (command[0].contentEquals("list")) {
+                    if (itemList.size() == 0) {
+                        IO.println("List is empty.");
+                        return;
+                    }
 
-                for (int i = 0; i < itemList.size(); i++) {
+                    for (int i = 0; i < itemList.size(); i++) {
+                        IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+                    }
+                } else if (command[0].contentEquals("mark")) {
+                    int i = Integer.parseInt(command[1]) - 1;
+                    itemList.get(i).mark();
                     IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+                } else if (command[0].contentEquals("unmark")) {
+                    int i = Integer.parseInt(command[1]) - 1;
+                    itemList.get(i).unmark();
+                    IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+                } else {
+                    throw new UnknownError("Unrecognized command");
                 }
-            } else if (command[0].contentEquals("mark")) {
-                int i = Integer.parseInt(command[1]) - 1;
-                itemList.get(i).mark();
-                IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
-            } else if (command[0].contentEquals("unmark")) {
-                int i = Integer.parseInt(command[1]) - 1;
-                itemList.get(i).unmark();
-                IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
-            } else {
-                IO.println("ERROR!");
+            } catch (IndexOutOfBoundsException e) {
+                IO.println(e);
+            } catch (IllegalArgumentException e) {
+                IO.println(e.getMessage());
+            } catch (UnknownError e) {
+                IO.println(e.getMessage());
             }
         }
     }
