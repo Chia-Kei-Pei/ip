0439c75ddde48d85ba646b4cc6be29342fe7a1d7
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-08-21 14:52:23 +0800

    Refactor: Removed wrapper class ItemList

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -1,7 +1,13 @@
+import datatypes.Deadline;
+import datatypes.Event;
+import datatypes.Todo;
+
+import java.util.ArrayList;
+
 public class Bert {
     public static void main(String[] args) {
         String[] command;
-        ItemList tasks = new ItemList();
+        ArrayList<Todo> itemList = new ArrayList<>();
 
         String banner = """
  ____     ___  ____  ______
@@ -20,88 +26,97 @@ public class Bert {
         while (true) {
             IO.println("\n------------------------------------------------------------");
             String userPrompt = IO.readln("> ");
+            IO.println("\n------------------------------------------------------------");
             command = userPrompt.split(" ");
             StringBuilder itemBuilder;
             String item;
-            String byDate;
-            String fromDate;
-            String toDate;
-            int i;
 
-            switch (command[0]) {
-                case "todo":
-                    itemBuilder = new StringBuilder();
-                    for (i = 1; i < command.length; i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    item = itemBuilder.toString().trim();
-                    IO.println("todo item: " + item);
-                    break;
-                case "deadline":
-                    i = 1;
-                    itemBuilder = new StringBuilder();
-                    for (; !command[i].contentEquals("/by") ; i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    item = itemBuilder.toString().trim();
-                    IO.println("deadline item: " + item);
+            if (command[0].contentEquals("todo")) {
+                itemBuilder = new StringBuilder();
+                for (int i = 1; i < command.length; i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                item = itemBuilder.toString().trim();
 
-                    i++;
-                    itemBuilder = new StringBuilder();
-                    for (; i < command.length; i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    byDate = itemBuilder.toString().trim();
-                    IO.println("byDate: " + byDate);
-                    break;
-                case "event":
-                    i = 1;
-                    itemBuilder = new StringBuilder();
-                    for (; !command[i].contentEquals("/from"); i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    item = itemBuilder.toString().trim();
-                    IO.println("event item: " + item);
+                Todo todo = new Todo(item);
+                itemList.add(todo);
+                IO.println("Added todo");
+                IO.println(todo);
+            } else if (command[0].contentEquals("deadline")) {
+                int i = 1;
+                itemBuilder = new StringBuilder();
+                for (; !command[i].contentEquals("/by"); i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                item = itemBuilder.toString().trim();
 
-                    i++;
-                    itemBuilder = new StringBuilder();
-                    for (; !command[i].contentEquals("/to"); i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    fromDate = itemBuilder.toString().trim();
-                    IO.println("fromDate: " + fromDate);
+                i++;
+                itemBuilder = new StringBuilder();
+                for (; i < command.length; i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                String byDate = itemBuilder.toString().trim();
 
-                    i++;
-                    itemBuilder = new StringBuilder();
-                    for (; i < command.length; i++) {
-                        itemBuilder.append(" ");
-                        itemBuilder.append(command[i]);
-                    }
-                    toDate = itemBuilder.toString().trim();
-                    IO.println("toDate: " + toDate);
-                    break;
-                case "bye":
-                case "exit":
-                case "quit":
-                    IO.println("\n------------------------------------------------------------");
-                    IO.println("Goodbye.");
+                Deadline deadline = new Deadline(item, byDate);
+                itemList.add(deadline);
+                IO.println("Added deadline");
+                IO.println(deadline);
+            } else if (command[0].contentEquals("event")) {
+                int i = 1;
+                itemBuilder = new StringBuilder();
+                for (; !command[i].contentEquals("/from"); i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                item = itemBuilder.toString().trim();
+
+                i++;
+                itemBuilder = new StringBuilder();
+                for (; !command[i].contentEquals("/to"); i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                String fromDate = itemBuilder.toString().trim();
+
+                i++;
+                itemBuilder = new StringBuilder();
+                for (; i < command.length; i++) {
+                    itemBuilder.append(" ");
+                    itemBuilder.append(command[i]);
+                }
+                String toDate = itemBuilder.toString().trim();
+
+                Event event = new Event(item, fromDate, toDate);
+                itemList.add(event);
+                IO.println("Added event");
+                IO.println(event);
+            } else if (command[0].contentEquals("bye")
+                    || command[0].contentEquals("exit")
+                    || command[0].contentEquals("quit")) {
+                IO.println("Goodbye.");
+                return;
+            } else if (command[0].contentEquals("list")) {
+                if (itemList.size() == 0) {
+                    IO.println("List is empty.");
                     return;
-                case "list":
-                    tasks.listItems();
-                    break;
-                case "mark":
-                    tasks.markItems(Integer.parseInt(command[1]));
-                    break;
-                case "unmark":
-                    tasks.unmarkItems(Integer.parseInt(command[1]));
-                    break;
-                default:
-                    IO.println("ERROR!");
+                }
+
+                for (int i = 0; i < itemList.size(); i++) {
+                    IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+                }
+            } else if (command[0].contentEquals("mark")) {
+                int i = Integer.parseInt(command[1]) - 1;
+                itemList.get(i).mark();
+                IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+            } else if (command[0].contentEquals("unmark")) {
+                int i = Integer.parseInt(command[1]) - 1;
+                itemList.get(i).unmark();
+                IO.println(String.format("%d.%s", i + 1, itemList.get(i).toString()));
+            } else {
+                IO.println("ERROR!");
             }
         }
     }
