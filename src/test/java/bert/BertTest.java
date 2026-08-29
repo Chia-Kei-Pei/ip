88ef345a603a92d3cf622352377223ac9e3733b0
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-08-29 13:45:12 +0800

    Prepare BertTest.java for actual test code

--- src/test/java/bert/BertTest.java
+++ src/test/java/bert/BertTest.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BertTest {
-    @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
-    }
+class BertTest {
 
+    /*
+     * Runs all possible cli commands that a user would normally use, expecting no errors.
+     */
     @Test
-    public void anotherDummyTest(){
-        assertEquals(4, 4);
+    void run_allCliCommands_noErrors() {
+        Bert bert = new Bert("data/todo_list.txt");
+        bert.run();
+
+        // write commands to cli
+
+        // read response from cli
+
     }
 }
