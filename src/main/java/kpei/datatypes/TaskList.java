5925b0b80851d945510c04b3f31e3d9575f02345
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-09-11 16:00:13 +0800

    TaskList.java: Rename 'todo' to 'task'

--- src/main/java/kpei/datatypes/TaskList.java
+++ src/main/java/kpei/datatypes/TaskList.java
@@ -13,7 +13,7 @@ public class TaskList {
     private String description;
 
     /**
-     * Initializes an empty todo list.
+     * Initializes an empty task list.
      */
     public TaskList(String description) {
         this.todos = new ArrayList<>();
@@ -27,10 +27,10 @@ public class TaskList {
     /**
      * Adds a task to the end of the list.
      *
-     * @param todo The task to add.
+     * @param task The task to add.
      */
-    public void add(Task todo) {
-        todos.add(todo);
+    public void add(Task task) {
+        todos.add(task);
     }
 
     /**
@@ -81,9 +81,9 @@ public class TaskList {
      * @throws InvalidIndexException If the index is outside the valid range.
      */
     public Task mark(int index) throws InvalidIndexException {
-        Task todo = get(index);
-        todo.mark();
-        return todo;
+        Task task = get(index);
+        task.mark();
+        return task;
     }
 
     /**
@@ -94,9 +94,9 @@ public class TaskList {
      * @throws InvalidIndexException If the index is outside the valid range.
      */
     public Task unmark(int index) throws InvalidIndexException {
-        Task todo = get(index);
-        todo.unmark();
-        return todo;
+        Task task = get(index);
+        task.unmark();
+        return task;
     }
 
     /**
@@ -127,9 +127,9 @@ public class TaskList {
     public TaskList find(String keyword) {
         TaskList matchingTasks = new TaskList("Matching tasks");
         String lowerKeyword = keyword.toLowerCase();
-        for (Task todo : todos) {
-            if (todo.getDescription().toLowerCase().contains(lowerKeyword)) {
-                matchingTasks.add(todo);
+        for (Task task : todos) {
+            if (task.getDescription().toLowerCase().contains(lowerKeyword)) {
+                matchingTasks.add(task);
             }
         }
         return matchingTasks;
