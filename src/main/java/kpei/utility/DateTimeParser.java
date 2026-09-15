ec1d947171e4d198cc9bbd888290de9def17a6ed
Chia-Kei-Pei <116048592+Chia-Kei-Pei@users.noreply.github.com>
2026-09-15 22:26:41 +0800

    Rename parser package to utility

--- src/main/java/kpei/utility/DateTimeParser.java
+++ src/main/java/kpei/utility/DateTimeParser.java
@@ -1,4 +1,4 @@
-package kpei.parser;
+package kpei.utility;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
--- src/test/java/kpei/utility/DateTimeParserTest.java
+++ src/test/java/kpei/utility/DateTimeParserTest.java
@@ -1,4 +1,4 @@
-package kpei.parser;
+package kpei.utility;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
